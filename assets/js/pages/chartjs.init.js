2fb116b2646ab0bd3ce0c63dca0a85acaa80c232
devdasz <devprasundas@outlook.com>
2024-01-30 12:58:03 +0530

    refactoring

--- assets/js/pages/chartjs.init.js
+++ assets/js/pages/chartjs.init.js
@@ -12,3 +12,37 @@ function getChartColorsArray(r) {
             })
         );
 }
+function getDataset(canvasId,label, data) {
+    lineChartColor = getChartColorsArray(canvasId);
+    return {
+                label:label,
+                fill: !0,
+                lineTension: 0.5,
+                backgroundColor: lineChartColor[0],
+                borderColor: lineChartColor[1],
+                borderCapStyle: "butt",
+                borderDash: [],
+                borderDashOffset: 0,
+                borderJoinStyle: "miter",
+                pointBorderColor: lineChartColor[1],
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: lineChartColor[1],
+                pointHoverBorderColor: "#fff",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: data.map((x) => x.value),
+            };
+}
+  // Default Chart Configuration
+  defaultOption = { x: { ticks: { font: { family: "Poppins" } } }, y: { ticks: { font: { family: "Poppins" } } }, plugins: { legend: { labels: { font: { family: "Poppins" } } } } };
+  function getData(canvasId,label, data){
+      return {
+          labels: data.map((x) => x.label),
+          datasets: [
+                                 getDataset(canvasId, label, data)
+            ],
+      };
+  }
